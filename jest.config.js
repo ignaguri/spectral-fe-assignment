33be93c24be25fad76cd44246750afec2ea53603
Ignacio Guri <ignacio.guri@mercadolibre.com>
2021-07-12 22:32:14 -0300

    add config files

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,13 @@ module.exports = {
   transform: {
     "^.+\\.vue$": "vue-jest",
   },
+  collectCoverage: true,
+  coveragePathIgnorePatterns: [
+    "<rootDir>/src/*/__tests__/",
+    "<rootDir>/src/config/",
+    "<rootDir>/node_modules/",
+  ],
+  coverageDirectory: "<rootDir>/coverage",
+  reporters: ["default"],
+  coverageReporters: ["html", "text", "text-summary"],
 };
